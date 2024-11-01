fce9f42a27b2f233d3eabd76c929e9c7bb9176eb
leezhenxue <zhenxue2880@gmail.com>
2024-11-01 18:54:46 +0800

    leave only code

--- Lab 1/L1Q3.java
+++ Lab 1/L1Q3.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.l1q3;
+//package com.mycompany.l1q3;
 
 /**
  *
--- Lab 1/MyProfile.java
+++ Lab 1/MyProfile.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package l1q2;
+package com.mycompany.l1q2;
 
 /**
  *
