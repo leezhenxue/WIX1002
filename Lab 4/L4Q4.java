2a5716aace1685c2af2e8c3c4bdfffa6551740b1
leezhenxue <zhenxue2880@gmail.com>
2024-11-04 18:34:11 +0800

    -

--- Lab 4/L4Q4.java
+++ Lab 4/L4Q4.java
@@ -3,37 +3,100 @@ public class L4Q4 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
-            int year, day, daytomay;
+            int year, day, num = 0;
 
-            System.out.println("Enter the year: ");
+            System.out.print("Enter the year: ");
             year = input.nextInt();
-            System.out.println("Enter the first day of the year: ");
+            System.out.print("Enter the first day of the year: ");
             day = input.nextInt();
 
             if (year % 4 == 0){
-                daytomay = 2;
+                day += 2;
             }
             else {
-                daytomay = 1;
+                day += 1;
             }
-
-            day += daytomay;
 
             if(day > 6){
                 day -= 7;
             }
             
-            System.out.println("Sun Mon Tue Wed Thu Fri Sat");
-            System.out.print("  ");
+
+            //May
+            System.out.println("\n Sun Mon Tue Wed Thu Fri Sat");
             for (int i = 0; i < day; i++){
                 System.out.print("    ");
             }
 
-            for (int i = 0; i < (7 - day); i++){
-                System.out.print((i+1) + "   ");
+            for (num = 1; num <= 31; num++){
+                System.out.printf("%4d", num);
+                if ((num + day) % 7 == 0){
+                    System.out.println();
+                }
             }
-            for (int i)
 
+            //June
+            day += 3;
+            if (day > 6){
+                day -= 7;
+            }
+        
+            System.out.println("\n Sun Mon Tue Wed Thu Fri Sat");
+            for (int i = 0; i < day; i++){
+                System.out.print("    ");
+            }
+
+            for (num = 1; num <= 30; num++){
+                System.out.printf("%4d", num);
+                if ((num + day) % 7 == 0){
+                    System.out.println();
+                }
+            }
+            /*System.out.println();
+
+            while(num <= 23){
+                for (int i = 0; i < 7; i++){
+                    num ++;
+                    System.out.printf("%4d", num);
+                }
+                System.out.println();
+            }
+
+            day = 31 - num;
+
+            for (int i = num; i < 31; i++){
+                num ++;
+                System.out.printf("%4d", num);
+            }
+
+            System.out.println();
+
+            //June
+            System.out.println("\n Sun Mon Tue Wed Thu Fri Sat");
+            for (int i = 0; i < day; i++){
+                System.out.print("    ");
+            }
+            
+            num = 0;
+
+            for (int i = 0; i < (7 - day); i++){
+                num += 1;
+                System.out.printf("%4d", num);
+            }
+            System.out.println();
+
+            while(num <= 22){
+                for (int i = 0; i < 7; i++){
+                    num ++;
+                    System.out.printf("%4d", num);
+                }
+                System.out.println();
+            }
+
+            for (int i = num; i < 30; i++){
+                num ++;
+                System.out.printf("%4d", num);
+            }*/
 
 
 
